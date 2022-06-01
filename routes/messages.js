4a0e5198dd96c9856bc6a2e193127117f5556975
lagar09 <ayala.lu91@gmail.com>
2022-05-31 22:49:05 -0300

    freshhh

--- routes/messages.js
+++ routes/messages.js
@@ -3,10 +3,10 @@
 */
 const {Router} = require('express');
 const { getChat } = require('../controllers/messages');
-// const validateJWT = require('../middlewares/validate-JWT');
+const validateJWT = require('../middlewares/validate-JWT');
 
 const router = Router();
 
-router.get('/:from', getChat);
+router.get('/:from',  validateJWT, getChat);
 
 module.exports = router;
