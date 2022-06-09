52b2898cba6cca36522917af480fec0970de378e
lagar09 <ayala.lu91@gmail.com>
2022-06-09 17:10:03 -0300

    Me fijo si el problema es en el validate

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -1,31 +1,31 @@
 const jwt = require('jsonwebtoken');
 
 const validateJWT = (req, res, next) => {
+    next();
+    // const token = req.header('x-token');
 
-    const token = req.header('x-token');
+    // if (!token) {
+    //     return res.status(401).json({
+    //         ok: false,
+    //         msg: 'No token'
+    //     });                        
+    // }
 
-    if (!token) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'No token'
-        });                        
-    }
+    // try {
 
-    try {
-
-        const {uid} = jwt.verify(token, process.env.JWT_KEY);
-        req.uid = uid;
+    //     const {uid} = jwt.verify(token, process.env.JWT_KEY);
+    //     req.uid = uid;
         
-        next();
+    //     next();
 
-    } catch (error) {
+    // } catch (error) {
 
-        return res.status(401).json({
-            ok: false,
-            msg: 'Invalid token'
-        });   
+    //     return res.status(401).json({
+    //         ok: false,
+    //         msg: 'Invalid token'
+    //     });   
         
-    }
+    // }
 };
 
 
